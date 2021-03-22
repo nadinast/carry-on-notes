df524e629857649246e8b47ba22a592b6505afc1
Nadina Streanga <nadistreanga@gmail.com>
2021-03-22 23:42:02 +0200

    Allowed CORS in production env

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
+var cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/carry-on-notes'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/carry-on-notes/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/carry-on-notes/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
